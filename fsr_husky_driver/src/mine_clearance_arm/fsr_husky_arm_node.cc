f62ee017d357854c1293ffa5fcd628058d925375
Gonçalo Cabrita <goncabrita@gmail.com>
2013-12-23 14:59:29 +0000

    Adding sweeping capabilities to the arm.

--- fsr_husky_driver/src/mine_clearance_arm/fsr_husky_arm_node.cc
+++ fsr_husky_driver/src/mine_clearance_arm/fsr_husky_arm_node.cc
@@ -6,6 +6,10 @@
 #include "jrk_driver.h"
 #include "nanotec_driver.h"
 
+#include <actionlib/server/simple_action_server.h>
+#include <fsr_husky_driver/HomeAction.h>
+#include <fsr_husky_driver/GotoAction.h>
+
 class FSRHuskyArm
 {
     public:
@@ -13,9 +17,15 @@ class FSRHuskyArm
         ~FSRHuskyArm();
 
         void init();
-        void home(double timeout=30.0);
+        bool home(double timeout=30.0);
         void setGoal(const sensor_msgs::JointState::ConstPtr& msg);
         void update();
+
+        actionlib::SimpleActionServer<fsr_husky_driver::HomeAction> m_as_home_;
+        void asHomeCallback(const fsr_husky_driver::HomeGoalConstPtr &req);
+
+        actionlib::SimpleActionServer<fsr_husky_driver::GotoAction> m_as_goto_;
+        void asGotoCallback(const fsr_husky_driver::GotoGoalConstPtr &goal);
 
     private:
         JRK jrk;
@@ -44,9 +54,19 @@ class FSRHuskyArm
         double max_linear_position_;
         double min_angular_position_;
         double max_angular_position_;
+
+        double linear_position_;
+        double angular_position_;
+        double linear_speed_;
+        double angular_speed_;
+
+        double goal_tolerance_;
+        ros::Duration timeout_;
 };
 
-FSRHuskyArm::FSRHuskyArm(ros::NodeHandle& node_handle, ros::NodeHandle& private_node_handle) : jrk(), m_node(node_handle), m_private_node(private_node_handle)
+FSRHuskyArm::FSRHuskyArm(ros::NodeHandle& node_handle, ros::NodeHandle& private_node_handle) : jrk(), m_node(node_handle), m_private_node(private_node_handle),
+    m_as_home_(node_handle, "fsr_husky_arm/home", boost::bind(&FSRHuskyArm::asHomeCallback, this, _1), false),
+    m_as_goto_(node_handle, "fsr_husky_arm/goto", boost::bind(&FSRHuskyArm::asGotoCallback, this, _1), false)
 {
     m_private_node.param<std::string>("base_frame_id", base_frame_id_, "arm_base_link");
     m_private_node.param<std::string>("linear_frame_id", linear_frame_id_, "arm_linear_link");
@@ -54,6 +74,11 @@ FSRHuskyArm::FSRHuskyArm(ros::NodeHandle& node_handle, ros::NodeHandle& private_
 
     m_private_node.param<std::string>("linear_joint", linear_joint_, "arm_linear_joint");
     m_private_node.param<std::string>("rotation_joint", rotation_joint_, "arm_rotation_joint");
+
+    m_private_node.param("goal_tolerance", goal_tolerance_, 0.01);
+    double timeout;
+    m_private_node.param("timeout", timeout, 20.0);
+    timeout_ = ros::Duration(timeout);
 }
 
 FSRHuskyArm::~FSRHuskyArm()
@@ -91,19 +116,33 @@ void FSRHuskyArm::init()
         ROS_BREAK();
     }
 
+    bool go_home;
+    m_private_node.param("home_at_startup", go_home, false);
+    if(go_home)
+    {
+        if(!home())
+        {
+            ROS_FATAL("FSR Husky Arm - %s - Failed to perform the homing routine!!", __FUNCTION__);
+            ROS_BREAK();
+        }
+    }
+
     m_joint_pub = m_node.advertise<sensor_msgs::JointState>("joint_states", 10);
     m_actuator_pub = m_node.advertise<sensor_msgs::JointState>("actuator_states", 10);
     m_joint_sub = m_node.subscribe<sensor_msgs::JointState>("cmd", 10, &FSRHuskyArm::setGoal, this);
+
+    m_as_home_.start();
+    m_as_goto_.start();
 }
 
-void FSRHuskyArm::home(double timeout)
+bool FSRHuskyArm::home(double timeout)
 {
     ROS_INFO("FSR Husky Arm - %s - Starting homing...", __FUNCTION__);
 
     if(!nanotec.startHoming())
     {
-        ROS_FATAL("FSR Husky Arm - %s - Failed to start the homing routine!", __FUNCTION__);
-        ROS_BREAK();
+        ROS_ERROR("FSR Husky Arm - %s - Failed to start the homing routine!", __FUNCTION__);
+        return false;
     }
 
     ros::Rate r(10.0);
@@ -114,22 +153,22 @@ void FSRHuskyArm::home(double timeout)
     }
     if(nanotec.getStatus() != NANOTEC_STATUS_ZERO)
     {
-        ROS_FATAL("FSR Husky Arm - %s - Timeout before the arm could complete the homing routine!", __FUNCTION__);
-        ROS_BREAK();
+        ROS_ERROR("FSR Husky Arm - %s - Timeout before the arm could complete the homing routine!", __FUNCTION__);
+        return false;
     }
 
     ros::Duration(1.0).sleep();
 
     if(!nanotec.setPositionMode())
     {
-        ROS_FATAL("FSR Husky Arm - %s - Failed to switch to absolute position mode!", __FUNCTION__);
-        ROS_BREAK();
+        ROS_ERROR("FSR Husky Arm - %s - Failed to switch to absolute position mode!", __FUNCTION__);
+        return false;
     }
 
     if(!nanotec.setPosition(5000))
     {
-        ROS_FATAL("FSR Husky Arm - %s - Failed to complete the homing routine!", __FUNCTION__);
-        ROS_BREAK();
+        ROS_ERROR("FSR Husky Arm - %s - Failed to complete the homing routine!", __FUNCTION__);
+        return false;
     }
 
     start_time = ros::Time::now();
@@ -139,24 +178,26 @@ void FSRHuskyArm::home(double timeout)
     }
     if(nanotec.getStatus() != NANOTEC_STATUS_LIMIT)
     {
-        ROS_FATAL("FSR Husky Arm - %s - Timeout before the arm could complete the homing routine!", __FUNCTION__);
-        ROS_BREAK();
+        ROS_ERROR("FSR Husky Arm - %s - Timeout before the arm could complete the homing routine!", __FUNCTION__);
+        return false;
     }
 
     if(!nanotec.clearPositionError())
     {
-        ROS_FATAL("FSR Husky Arm - %s - Failed to complete the homing routine!", __FUNCTION__);
-        ROS_BREAK();
+        ROS_ERROR("FSR Husky Arm - %s - Failed to complete the homing routine!", __FUNCTION__);
+        return false;
     }
 
     if(!nanotec.getPosition(max_position_))
     {
-        ROS_FATAL("FSR Husky Arm - %s - Failed to complete the homing routine!", __FUNCTION__);
-        ROS_BREAK();
+        ROS_ERROR("FSR Husky Arm - %s - Failed to complete the homing routine!", __FUNCTION__);
+        return false;
     }
     ROS_INFO("FSR Husky Arm - %s - Limit reached at %d steps.", __FUNCTION__, max_position_);
 
     ROS_INFO("FSR Husky Arm - %s - Homing complete!", __FUNCTION__);
+
+    return true;
 }
 
 void FSRHuskyArm::setGoal(const sensor_msgs::JointState::ConstPtr& msg)
@@ -184,8 +225,8 @@ void FSRHuskyArm::update()
         return;
     }
     ros::Duration delta_t = ros::Time::now() - last_update_;
-    double l = linear_position*(max_linear_position_ - min_linear_position_)/4048 + min_linear_position_;
-    double linear_speed = (l - last_linear_position_)/delta_t.toSec();
+    linear_position_ = linear_position*(max_linear_position_ - min_linear_position_)/4048 + min_linear_position_;
+    linear_speed_ = (linear_position_ - last_linear_position_)/delta_t.toSec();
 
     int rotation_position;
     if(!nanotec.getPosition(rotation_position))
@@ -194,8 +235,8 @@ void FSRHuskyArm::update()
         return;
     }
     delta_t = ros::Time::now() - last_update_;
-    double a = rotation_position*(max_angular_position_ - min_angular_position_)/max_position_ + min_angular_position_;
-    double rotation_speed = (a - last_rotation_position_)/delta_t.toSec();
+    angular_position_ = rotation_position*(max_angular_position_ - min_angular_position_)/max_position_ + min_angular_position_;
+    angular_speed_ = (angular_position_ - last_rotation_position_)/delta_t.toSec();
 
     sensor_msgs::JointState joint_state;
     ros::Time now = ros::Time::now();
@@ -217,22 +258,102 @@ void FSRHuskyArm::update()
     joint_state.position.resize(4);
     joint_state.velocity.resize(4);
     joint_state.name[0] = "arm_axel_joint";
-    joint_state.position[0] = a;
-    joint_state.velocity[0] = rotation_speed;
+    joint_state.position[0] = angular_position_;
+    joint_state.velocity[0] = angular_speed_;
     joint_state.name[1] = "upper_arm_joint";
-    joint_state.position[1] = l;
-    joint_state.velocity[1] = linear_speed;
+    joint_state.position[1] = linear_position_;
+    joint_state.velocity[1] = linear_speed_;
     joint_state.name[2] = "lower_arm_joint";
-    joint_state.position[2] = l;
-    joint_state.velocity[2] = linear_speed;
+    joint_state.position[2] = linear_position_;
+    joint_state.velocity[2] = linear_speed_;
     joint_state.name[3] = "metal_detector_arm_joint";
-    joint_state.position[3] = -l;
-    joint_state.velocity[3] = linear_speed;
+    joint_state.position[3] = -linear_position_;
+    joint_state.velocity[3] = linear_speed_;
     m_joint_pub.publish(joint_state);
 
-    last_update_ = ros::Time::now();
-    last_linear_position_ = l;
-    last_rotation_position_ = a;
+    last_update_ = now;
+    last_linear_position_ = linear_position_;
+    last_rotation_position_ = angular_position_;
+}
+
+void FSRHuskyArm::asHomeCallback(const fsr_husky_driver::HomeGoalConstPtr &req)
+{
+    if(home())
+    {
+         m_as_home_.setSucceeded();
+    }
+    else
+    {
+        m_as_home_.setAborted();
+    }
+}
+
+void FSRHuskyArm::asGotoCallback(const fsr_husky_driver::GotoGoalConstPtr &goal)
+{
+    ros::Rate loop_rate(10.0);
+
+    ros::Time start_time = ros::Time::now();
+
+    double goal_l = goal->joint.position[0];
+    double goal_a = goal->joint.position[1];
+    double peed_l = goal->joint.velocity[0];
+    double speed_a = goal->joint.velocity[1];
+
+    jrk.setPosition(goal_l);
+    nanotec.setPosition(goal_a);
+
+    // start executing the action
+    while(ros::ok())
+    {
+        // check that preempt has not been requested by the client
+        if(m_as_goto_.isPreemptRequested())
+        {
+            ROS_INFO("FSR Husky Arm - %s - Goal preempted", __FUNCTION__);
+            // set the action state to preempted
+            m_as_goto_.setPreempted();
+            break;
+        }
+
+        // Read Position & Speed
+        sensor_msgs::JointState joint_state;
+        joint_state.header.stamp = ros::Time::now();
+        joint_state.header.frame_id = "fsr_husky_arm";
+        joint_state.name.resize(2);
+        joint_state.position.resize(2);
+        joint_state.velocity.resize(2);
+        joint_state.name[0] = linear_joint_;
+        joint_state.position[0] = linear_position_;
+        joint_state.velocity[0] = linear_speed_;
+        joint_state.name[1] = rotation_joint_;
+        joint_state.position[1] = angular_position_;
+        joint_state.velocity[1] = angular_speed_;
+
+        if(ros::Time::now() - start_time > timeout_)
+        {
+            fsr_husky_driver::GotoResult result;
+            result.joint = joint_state;
+            ROS_INFO("FSR Husky Arm - %s - Goal timeout", __FUNCTION__);
+            // set the action state to failed
+            m_as_goto_.setAborted();
+            break;
+        }
+
+        if( fabs(goal->joint.position[0] - linear_position_) <= goal_tolerance_  && fabs(goal->joint.position[1] - angular_position_) <= goal_tolerance_ )
+        {
+            fsr_husky_driver::GotoResult result;
+            result.joint = joint_state;
+            // set the action state to succeeded
+            m_as_goto_.setSucceeded(result);
+            break;
+        }
+
+        fsr_husky_driver::GotoFeedback feedback;
+        feedback.joint = joint_state;
+        // publish the feedback
+        m_as_goto_.publishFeedback(feedback);
+
+        loop_rate.sleep();
+    }
 }
 
 int main(int argc, char** argv)
@@ -245,7 +366,6 @@ int main(int argc, char** argv)
 
     FSRHuskyArm arm(n, pn);
     arm.init();
-    arm.home();
 
     int rate;
     pn.param("rate", rate, 20);
